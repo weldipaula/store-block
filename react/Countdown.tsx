666ee32218f855462816d56a24f8b206843ca81f
weldipaula <58184147+weldipaula@users.noreply.github.com>
2020-08-15 22:13:34 -0300

    Update Countdown.tsx

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,1 +1,21 @@
- 
+import React from 'react'
+
+interface CountdownProps { }
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+  return <div></div>
+  return (
++      <div>
++        <h1>Teste Countdown</h1>
++      </div>
++    )
+}
+
+Countdown.schema = {
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {},
+}
+
+export default Countdown
